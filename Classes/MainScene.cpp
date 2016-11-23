d30d3d25fc3f0b6e5a2a74df25a33dde40f5748b
A <triplecode@kookmin.ac.kr>
2016-11-23 12:57:50 +0900

    test cutting

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -10,11 +10,56 @@ bool MainScene::init()
 	Vec2 center = Director::getInstance()->getVisibleSize() / 2;
 	int size = 200;
 
-	Vec2 cut = Vec2(size / 2, 0) - Vec2(-size / 2, -size / 2);
+	Vec2 cut1 = Vec2(-size / 2, -size / 2);
+	Vec2 cut2 = Vec2(size / 2, 0);
+	//Vec2 cut = Vec2(size / 2, 0) - Vec2(-size / 2, -size / 2);
 
 	Vec2 r = symmetry(0.5f, -50, Vec2(size / 2, -size / 2));
 
-	auto r1 = DrawNode::create();
+	DrawNode *draw;
+
+	Vec2 *v = new Vec2[4];
+
+	////////////////
+
+	v[0] = Vec2(-size / 2, -size / 2);
+	v[1] = Vec2(size / 2, -size / 2);
+	v[2] = Vec2(size / 2, size / 2);
+	v[3] = Vec2(-size / 2, size / 2);
+
+	draw = DrawNode::create();
+	draw->setPosition(center);
+
+	v[0] = cut1;
+	v[1] = cut2;
+
+	draw->drawSolidPoly(v, 4, Color4F::RED);
+
+	addChild(draw);
+
+	delete[] v;
+
+	////////////////
+
+	v[0] = Vec2(-size / 2, -size / 2);
+	v[1] = Vec2(size / 2, -size / 2);
+	v[2] = Vec2(size / 2, size / 2);
+	v[3] = Vec2(-size / 2, size / 2);
+
+	draw = DrawNode::create();
+	draw->setPosition(center);
+	 
+	//v[1] = symmetry(0.5, -50, v[0]);
+	v[2] = symmetry(0.5, -50, Vec2(size / 2, -size / 2));
+	v[3] = symmetry(0.5, -50, v[0]);
+
+	draw->drawSolidPoly(v, 4, Color4F(0, 0, 1, 0.5));
+
+	addChild(draw);
+
+	delete[] v;
+
+	/*auto r1 = DrawNode::create();
 	r1->setPosition(center);
 
 	Vec2 *v = new Vec2[4];
@@ -44,7 +89,7 @@ bool MainScene::init()
 
 	addChild(r2);
 
-	delete[] v;
+	delete[] v;*/
 
 
 	return true;
@@ -52,5 +97,5 @@ bool MainScene::init()
 
 Vec2 MainScene::symmetry(float a, float b, const Vec2 & point)
 {
-	return Vec2((2 * a * point.y - (a * a - 1) * point.x - 2 * a * b) / (a * a + 1), -(2 * a * point.x - (a * a - 1) * point.y + 2 * b) / (a * a + 1));
+	return Vec2((2 * a * point.y - (a * a - 1) * point.x - 2 * a * b) / (a * a + 1), (2 * a * point.x + (a * a - 1) * point.y + 2 * b) / (a * a + 1));
 }
