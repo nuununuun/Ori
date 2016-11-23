55aa54cdce1fc010ce81768d0e24d98e6a203062
nuununuun <triplecode@kookmin.ac.kr>
2016-11-23 14:10:35 +0900

    test touchPoint

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -5,10 +5,17 @@ USING_NS_CC;
 bool MainScene::init()
 {
 	if (!Layer::init()) return false;
-
+    
+    auto bg = LayerColor::create(Color4B(255, 255, 255, 255));
+    addChild(bg);
 
 	Vec2 center = Director::getInstance()->getVisibleSize() / 2;
 	int size = 200;
+    
+    Vec2 touchPoint = Vec2(size / 2 - 50, -size / 2);
+    Vec2 endPoint = Vec2(size / 2 - 120, -size / 2 + 60);
+    
+    (endPoint - touchPoint).getPerp();
 
 	Vec2 cut1 = Vec2(-size / 2, -size / 2);
 	Vec2 cut2 = Vec2(size / 2, 0);
@@ -29,68 +36,37 @@ bool MainScene::init()
 
 	draw = DrawNode::create();
 	draw->setPosition(center);
-
-	v[0] = cut1;
-	v[1] = cut2;
-
-	draw->drawSolidPoly(v, 4, Color4F::RED);
+//
+//	v[0] = cut1;
+//	v[1] = cut2;
+    
+    draw->drawPolygon(v, 4, Color4F(1, 0.236, 0.148, 1), 0.4, Color4F(0.6, 0.15, 0.07, 1));
+    draw->drawPoint(touchPoint, 12, Color4F(0, 0, 1, 1));
+    draw->drawPoint(endPoint, 12, Color4F(0, 1, 0, 1));
 
 	addChild(draw);
 
-	delete[] v;
+//	delete[] v;
 
 	////////////////
 
-	v[0] = Vec2(-size / 2, -size / 2);
-	v[1] = Vec2(size / 2, -size / 2);
-	v[2] = Vec2(size / 2, size / 2);
-	v[3] = Vec2(-size / 2, size / 2);
+//	v[0] = Vec2(-size / 2, -size / 2);
+//	v[1] = Vec2(size / 2, -size / 2);
+//	v[2] = Vec2(size / 2, size / 2);
+//	v[3] = Vec2(-size / 2, size / 2);
+//
+//	draw = DrawNode::create();
+//	draw->setPosition(center);
+//	 
+//	v[1] = symmetry(0.5, -50, cut2);
+//	v[2] = symmetry(0.5, -50, Vec2(size / 2, -size / 2));
+//	v[3] = symmetry(0.5, -50, v[0]);
+//
+//	draw->drawPolygon(v, 4, Color4F(0.854, 0.236, 0.118, 1), 0.4, Color4F(0.6, 0.15, 0.07, 1));
+//
+//	addChild(draw);
 
-	draw = DrawNode::create();
-	draw->setPosition(center);
-	 
-	//v[1] = symmetry(0.5, -50, v[0]);
-	v[2] = symmetry(0.5, -50, Vec2(size / 2, -size / 2));
-	v[3] = symmetry(0.5, -50, v[0]);
-
-	draw->drawSolidPoly(v, 4, Color4F(0, 0, 1, 0.5));
-
-	addChild(draw);
-
-	delete[] v;
-
-	/*auto r1 = DrawNode::create();
-	r1->setPosition(center);
-
-	Vec2 *v = new Vec2[4];
-
-	v[0] = Vec2(-size / 2, -size / 2);
-	v[1] = Vec2(size / 2, 0);
-	v[2] = Vec2(size / 2, size / 2);
-	v[3] = Vec2(-size / 2, size / 2);
-
-	r1->drawSolidPoly(v, 4, Color4F(1, 0, 0, 1));
-
-	addChild(r1);
-
-	delete[] v;
-
-	auto r2 = DrawNode::create();
-	r2->setPosition(center);
-
-	v = new Vec2[4];
-
-	v[0] = Vec2(-size / 2, -size / 2);
-	v[1] = Vec2(size / 2, 0);
-	v[2] = r;
-	v[3] = Vec2(-size / 2, -size / 2);
-
-	r2->drawSolidPoly(v, 4, Color4F(0, 0, 1, 1));
-
-	addChild(r2);
-
-	delete[] v;*/
-
+//	delete[] v;
 
 	return true;
 }
